1eb627f7bea6303746e04d86898380c6b8fe994f
Genepro75 <eugene.otuya@student.moringaschool.com>
2025-09-30 13:51:19 +0300

    Added titleCased tutorials project

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(' ');
+
+    return words.map(word => {
+      if (word.length === 0) return word;
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
